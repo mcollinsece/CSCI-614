f8c308b967664a1e2170cea4de9c3e1bbdcd377e
Matthew <matthewcollinsece@gmail.com>
2024-04-15 12:20:40 -0400

    Initialize Assignment 4

---
+++ assignment_4/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>    /* for printf */
+#include <stdlib.h>   /* for string to integer conversion, random numbers */
+#include "mem.h"
+
+/*
+  The main program will accept four paramemters on the command line.
+  The first parameter is the memory size.  The second parameter is the
+  duration of the each simulation run.  The third parameter is the
+  number of times to repeat each experiment (ie. number of runs). The
+  fourth parameter is a random number seed. Here is an example command
+  line:
+
+  ./hw7 1000 3000 100 1235
+
+  This means that your program should initialize physical memory to
+  1,000 units, perform 100 runs with each run taking 3000 units of
+  time, and the random number generator should be seeded (one time)
+  with the value 1235.
+*/
+
+int main(int argc, char** argv)
+{  
+  return 0;
+}
