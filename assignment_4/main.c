ea5f9df1bc303f62c8f66e745a2c62d0f13819fe
Matthew <matthewcollinsece@gmail.com>
2024-04-15 16:07:31 -0400

    Current implentation of Assignment 4, 4_15

--- assignment_4/main.c
+++ assignment_4/main.c
@@ -1,24 +1,59 @@
-#include <stdio.h>    /* for printf */
-#include <stdlib.h>   /* for string to integer conversion, random numbers */
+#include <stdio.h>
+#include <stdlib.h>
 #include "mem.h"
 
-/*
-  The main program will accept four paramemters on the command line.
-  The first parameter is the memory size.  The second parameter is the
-  duration of the each simulation run.  The third parameter is the
-  number of times to repeat each experiment (ie. number of runs). The
-  fourth parameter is a random number seed. Here is an example command
-  line:
 
-  ./hw7 1000 3000 100 1235
+int main(int argc, char** argv) {
+    if (argc != 5) {
+        printf("Usage: %s <memory size> <duration> <runs> <seed>\n", argv[0]);
+        return 1;
+    }
 
-  This means that your program should initialize physical memory to
-  1,000 units, perform 100 runs with each run taking 3000 units of
-  time, and the random number generator should be seeded (one time)
-  with the value 1235.
-*/
 
-int main(int argc, char** argv)
-{  
-  return 0;
+    mem_strats_t current_strategy;   // Declaration
+    current_strategy = BESTFIT;     // Initialization inside main()
+    printf("Current strategy: %d\n", current_strategy);
+
+    int mem_size = atoi(argv[1]);
+    int duration = atoi(argv[2]);
+    int runs = atoi(argv[3]);
+    int seed = atoi(argv[4]);
+
+    srand(seed);
+    mem_init(mem_size);
+
+    double total_failures = 0, total_fragments = 0, total_probes = 0;
+
+    for (int run = 0; run < runs; run++) {
+        mem_clear();
+        int failures = 0, probes = 0;
+
+        for (int time_unit = 0; time_unit < duration; time_unit++) {
+            int size = MIN_REQUEST_SIZE + (rand() % (MAX_REQUEST_SIZE - MIN_REQUEST_SIZE + 1));
+            dur_t alloc_duration = MIN_DURATION + (rand() % (MAX_DURATION - MIN_DURATION + 1));
+            //int result = mem_allocate(FIRSTFIT, size, alloc_duration);
+            int result = mem_allocate(current_strategy, size, alloc_duration);
+            if (result == -1) {
+                failures++;
+            } else {
+                probes += result;
+            }
+
+            mem_single_time_unit_transpired();
+        }
+
+        total_failures += failures;
+        total_fragments += mem_fragment_count(10);  // Example fragment size threshold
+        total_probes += probes;
+    }
+
+    printf("Strategy | Average Failures | Average Fragments | Average Probes\n");
+    printf("%d | %f | %f | %f\n",
+           current_strategy,
+           total_failures / runs,
+           total_fragments / runs,
+           total_probes / runs);
+
+    mem_free();
+    return 0;
 }
--- assignment_4/mem.c
+++ assignment_4/mem.c
@@ -18,7 +18,7 @@ static int mem_size;
 
 /*
  The last_placement_position variable contains the end position of the
- last allocated unit used by the next fit placement algorithm.
+ last allocated unit used by the next fit placement algorithm. Uncomment if using NEXTFIT
  */
 static int last_placement_position;
 
@@ -34,33 +34,168 @@ static int last_placement_position;
   If a suitable contiguous block of memory is found, the first size
   units of this block must be set to the value, duration.
  */
-int mem_allocate(mem_strats_t strategy, int size, dur_t duration)
-{
+
+int mem_allocate(mem_strats_t strategy, int size, dur_t duration) {
+    int start = last_placement_position;
+    int probes = 0;
+
+    if (strategy == NEXTFIT) {
+        do {
+            if (memory[start] == 0) {  // Found free memory
+                int j;
+                for (j = start; j < start + size && j < mem_size; j++) {
+                    if (memory[j] != 0) break;
+                }
+                if (j == start + size) {  // Suitable block found
+                    for (j = start; j < start + size; j++) memory[j] = duration;
+                    last_placement_position = (j == mem_size) ? 0 : j;  // Update the last placement
+                    return probes;
+                }
+            }
+            probes++;
+            start = (start + 1) % mem_size;  // Wrap around
+        } while (start != last_placement_position);  // Stop if we've checked the whole memory
+    }   
+    if (strategy == BESTFIT) {
+        int best_start = -1;
+        int min_size = mem_size + 1;  // Larger than any possible block
+        int current_start = -1;
+
+        for (int i = 0; i < mem_size; i++) {
+            if (memory[i] == 0) {
+                if (current_start == -1) current_start = i;  // Mark the start of a free block
+            } else if (current_start != -1) {
+                // End of a free block found
+                int block_size = i - current_start;
+                if (block_size >= size && block_size < min_size) {
+                    best_start = current_start;
+                    min_size = block_size;
+                }
+                current_start = -1;  // Reset start marker
+            }
+            probes++;
+        }
+
+        // Check last block if it ends at the end of memory
+        if (current_start != -1) {
+            int block_size = mem_size - current_start;
+            if (block_size >= size && block_size < min_size) {
+                best_start = current_start;
+                min_size = block_size;
+            }
+        }
+
+        if (best_start != -1) {
+            for (int j = best_start; j < best_start + size; j++) {
+                memory[j] = duration;
+            }
+            return probes;
+        }
+    } else if (strategy == FIRSTFIT) {
+        // Implement FIRSTFIT logic here, if still needed
+        for (int i = 0; i < mem_size; i++) {
+            if (memory[i] == 0) { // Found free memory
+                int j;
+                for (j = i; j < i + size; j++) {
+                    if (j >= mem_size || memory[j] != 0) {
+                        break;
+                    }
+                }
+                if (j == i + size) {  // Suitable block found
+                    for (j = i; j < i + size; j++) memory[j] = duration;
+                    return probes;
+                }
+            }
+            probes++;
+        }
+    }
+
+    return -1;  // No suitable block found
 }
+
+
+
+
+
+
+
+
+// int mem_allocate(mem_strats_t strategy, int size, dur_t duration) {
+//     int i, probes = 0;
+//     if (strategy == FIRSTFIT) {
+//         for (i = 0; i < mem_size; i++) {
+//             if (memory[i] == 0) { // Found free memory
+//                 int j, fit = 1;
+//                 for (j = i; j < i + size; j++) {
+//                     if (j >= mem_size || memory[j] != 0) {
+//                         fit = 0;
+//                         break;
+//                     }
+//                 }
+//                 if (fit) {
+//                     for (j = i; j < i + size; j++) memory[j] = duration;
+//                     return probes;
+//                 }
+//             }
+//             probes++;
+//         }
+//     }
+//     return -1;
+// }
 
 /*
   Go through all of memory and decrement all positive-valued entries.
   This simulates one unit of time having transpired.  NOTE: when a
   memory cell is decremented to zero, it becomes "unallocated".
  */
-int mem_single_time_unit_transpired()
-{
+int mem_single_time_unit_transpired() {
+    int i, freed_blocks = 0;
+    for (i = 0; i < mem_size; i++) {
+        if (memory[i] > 0) {
+            memory[i]--;
+            if (memory[i] == 0) {
+                freed_blocks++;
+                // Further logic if needed to handle larger freed blocks
+            }
+        }
+    }
+    return freed_blocks;  // Return the number of blocks that became free
 }
+
 
 /*
   Return the number of fragments in memory.  A fragment is a
   contiguous free block of memory of size less than or equal to
   frag_size.
  */
-int mem_fragment_count(int frag_size)
-{
+int mem_fragment_count(int frag_size) {
+    int i = 0, fragment_count = 0;
+
+    while (i < mem_size) {
+        if (memory[i] == 0) { // start of a free block
+            int block_size = 0;
+            while (i < mem_size && memory[i] == 0) {
+                block_size++;
+                i++;
+            }
+            if (block_size <= frag_size) {
+                fragment_count++;
+            }
+        } else {
+            i++;
+        }
+    }
+    return fragment_count;  // Ensure that this function always returns a value.
 }
+
 
 /*
   Set the value of zero to all entries of memory.
  */
-void mem_clear()
-{
+void mem_clear() {
+    for (int i = 0; i < mem_size; i++) {
+        memory[i] = 0;
+    }
 }
 
 /*
@@ -91,6 +226,15 @@ void mem_free()
   blocks, rather than single units; otherwise, the output will be very
   long.
  */
-void mem_print()
-{
+void mem_print() {
+    printf("Memory Dump:\n");
+    for (int i = 0; i < mem_size; ) {
+        int current_value = memory[i];
+        int start = i;
+        while (i < mem_size && memory[i] == current_value) {
+            i++;
+        }
+        printf("Block from %d to %d -> Value: %d, Size: %d\n", start, i - 1, current_value, i - start);
+    }
+    printf("\n");
 }
